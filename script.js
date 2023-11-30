a56f500616267bb9319c7d0a7ff961effaeb20f3
iamsamirtagiyev <stagiyev579@gmail.com>
2023-11-30 11:10:29 +0400

    toystore

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const cartBtn = document.querySelector('.nav .right')
 const cartwrapper = document.querySelector('.cart-wrapper')
 const cartModal = document.querySelector('.cart-modal')
 const closeBtn = document.querySelector('.bx-x')
+const menuIcon = document.querySelector('.bx-menu')
+const menu = document.querySelector('.menu')
+const nav = document.querySelector('nav')
 
 document.addEventListener('DOMContentLoaded', ()=> modal.classList.add('active'))
 
@@ -14,4 +17,25 @@ cartBtn.addEventListener('click', ()=> {
 closeBtn.addEventListener('click', ()=>{
     cartModal.classList.remove('active')
     cartwrapper.classList.remove('active')
+})
+
+menuIcon.addEventListener('click', ()=>{
+    menu.classList.toggle('active')
+})
+
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY > 30){
+        nav.style.background = 'black'
+        nav.style.height = '60px'
+        nav.querySelector('.logo').style.color = 'white'
+        nav.querySelectorAll('li').forEach(element => element.classList.add('active'))
+        nav.querySelector('.right').style.color = 'white'
+    }
+    else{
+        nav.style.background = 'white' 
+        nav.style.height = '80px'
+        nav.querySelector('.logo').style.color = 'black'
+        nav.querySelectorAll('li').forEach(element => element.classList.remove('active'))
+        nav.querySelector('.right').style.color = 'black'
+    }
 })
